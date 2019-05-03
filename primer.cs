9cf456b19fb288f448516d23f576af85f58a3cb8
Jason Lu <jasonlu6@bu.edu>
2019-05-02 21:59:36 -0400

    Simple coding examples using C# for Unity AR/VR
    Tutorial #1: Hello World program (primer.cs)
    
    Tutorial #2: Example using RGB color imaging / scripts as behavior components (ExampleBehaviourScirpt.cs) 
    
    Followed an online tutorial for Unity): 
    
    Source:https://learn.unity.com/tutorial/scripts-as-behaviour-components?
    projectId=5c8920b4edbc2a113b6bc26a&playlist=17117#5c8924ededbc2a113b6bc373
    
    Tutorial #3: Arithmetic OOP examples with addition, subtraction, multiplication, division (VariablesAndFunctions.cs)

---
+++ primer.cs
@@ -0,0 +1,21 @@
+﻿/* Generic first program in C# 
+Author: Jason Lu (lu.jaso@husky.neu.edu) 
+Date: 5.01.2019
+Training Module #1 for NEU Research Summer 2019 
+*/
+
+using System;
+namespace Application
+{
+    class primer
+    {
+        static void Main(string[] args)
+        {
+            /* "print" statement for C# */
+            Console.WriteLine("Hello World");
+            Console.ReadKey();
+            /* Exit gracefully out of the environment */
+            System.Environment.Exit(1);
+        }
+    }
+}
